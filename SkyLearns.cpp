85c8084ceb1737d88b863ee3db79bb0403134b3a
Grep2002 <Jasminemarieamber@gmail.com>
2017-02-02 19:47:22 -0400

    First file

---
+++ SkyLearns.cpp
@@ -0,0 +1,18 @@
+// #include "stdfax.h"
+#include <iostream>
+
+using namespace std
+
+int x;
+
+int main()
+{
+      cout << "Enter a number" << endl;
+      cin >> x;
+      cout << "Your number is " << x << endl;
+
+      system("pause");
+
+      return 0;
+      
+}
